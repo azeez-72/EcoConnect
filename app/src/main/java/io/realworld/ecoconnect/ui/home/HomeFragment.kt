23f0f76b48cc66d5f1b104a56bfb439106de9da8
sans2801 <thakareprasad28@yahoo.in>
2021-03-10 17:01:25 +0530

    updated login signup

--- app/src/main/java/io/realworld/ecoconnect/ui/home/HomeFragment.kt
+++ app/src/main/java/io/realworld/ecoconnect/ui/home/HomeFragment.kt
@@ -1,13 +1,17 @@
 package io.realworld.ecoconnect.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import io.realworld.ecoconnect.LoginSignUpActivity
 import io.realworld.ecoconnect.R
 
 class HomeFragment : Fragment() {
@@ -22,10 +26,14 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        root.findViewById<Button>(R.id.signoutButton).setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val loginSignupIntent = Intent(activity, LoginSignUpActivity::class.java)
+            loginSignupIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(loginSignupIntent)
+        }
+
         return root
     }
 }
